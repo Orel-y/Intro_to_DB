4b9e72edc12c4c192613ca9ec87fbca9ceebe132
Yoseph won <yosephwondimu26@gmail.com>
2025-08-17 13:48:09 +0300

    checker :(

--- task_2.sql
+++ task_2.sql
@@ -29,11 +29,11 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS `order details`(
+CREATE TABLE IF NOT EXISTS Order_Details(
 	orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
